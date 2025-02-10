74450b7618a8eaf34f23cd8ded21e6243c909199
Afsaneh Hassani <afsaneh_hassani67@yahoo.com>
2025-02-10 13:18:58 +0330

    url parameters
    the relevant urls to blog and one-pages

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from blog.models import Post
+import datetime
 
 # Create your views here.
 def blog_view(request):
-    return render(request,'blog/blog-home.html')
+    posts=Post.objects.filter(published_date__lte=datetime.datetime.now())
+    context={'posts': posts}
+    return render(request,'blog/blog-home.html',context)
 
 def blog_single(request):
     return render(request,'blog/blog-single.html')
+
+def test(request,pid):
+    post=get_object_or_404(Post,pk=pid)
+    post.counted_view=post.counted_view+1
+    post.save()
+    context={'post': post}
+    return render(request,'test.html',context)
