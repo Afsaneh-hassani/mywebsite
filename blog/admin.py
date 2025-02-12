d71600ea45847f34d11730211c0466ba4c6b50f9
Afsaneh Hassani <afsaneh_hassani67@yahoo.com>
2025-02-12 23:24:54 +0330

    adding the field of category and author
    adding the field of category and author

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from blog.models import Post
+from blog.models import Post, Category
 
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy='created_date'
     empty_value_display='-empty-'
-    list_display=('title','counted_view','status','published_date','created_date')
-    list_filter=('status',)
+    list_display=('title','author','counted_view','status','published_date','created_date')
+    list_filter=('status','author')
     #ordering=['-created_date']
     search_fields=['title','content']
     
@@ -17,3 +17,4 @@ class PostAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Post,PostAdmin)
+admin.site.register(Category)
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post
-import datetime
+from django.utils import timezone
 
 # Create your views here.
 def blog_view(request):
-    posts=Post.objects.filter(published_date__lte=datetime.datetime.now())
+    posts=Post.objects.filter(published_date__lte=timezone.now(),status=1)
     context={'posts': posts}
     return render(request,'blog/blog-home.html',context)
 
-def blog_single(request):
-    return render(request,'blog/blog-single.html')
-
-def test(request,pid):
-    post=get_object_or_404(Post,pk=pid)
+def blog_single(request,pid):
+    posts=Post.objects.filter(status=1)
+    post=get_object_or_404(posts,pk=pid)
     post.counted_view=post.counted_view+1
     post.save()
     context={'post': post}
-    return render(request,'test.html',context)
+    return render(request,'blog/blog-single.html',context)
+
