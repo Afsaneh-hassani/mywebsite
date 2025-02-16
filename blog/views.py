3a288690e8ba846541a307264c8e1c505aebf019
Afsaneh Hassani <afsaneh_hassani67@yahoo.com>
2025-02-16 18:00:57 +0330

    correction the single_blog
    correction the single_blog

--- blog/views.py
+++ blog/views.py
@@ -14,8 +14,8 @@ def blog_single(request,pid):
     posts=Post.objects.filter(status=1)
     post=get_object_or_404(posts ,pk=pid)
     
-    previous_post=Post.objects.filter(created_date__gt=post.created_date).order_by('created_date').first()
-    next_post=Post.objects.filter(created_date__lt=post.created_date).order_by('-created_date').first()
+    previous_post=Post.objects.filter(created_date__gt=post.created_date,published_date__lte=timezone.now(),status=1 ).order_by('created_date').first()
+    next_post=Post.objects.filter(created_date__lt=post.created_date,published_date__lte=timezone.now(),status=1 ).order_by('-created_date').first()
     
     post.counted_view=post.counted_view+1
     post.save()
