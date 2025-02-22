0d3e113c253237839129cd7111d18e1341c8b745
Afsaneh Hassani <afsaneh_hassani67@yahoo.com>
2025-02-22 22:13:11 +0330

    filtering
    filtering posts according to
    -author name
    -search form

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('', blog_view, name='index'),
     path('<int:pid>', blog_single, name='single'),
     #path('<int:pid>', test, name='test'),
+    path('category/<str:cat_name>', blog_view, name='category'),
+    path('author/<str:author_username>', blog_view, name='author'),
     path('test',test, name='test'),
+    path('search/', blog_search, name='search'),
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,13 @@ from blog.models import Post
 from django.utils import timezone
 
 # Create your views here.
-def blog_view(request):
+def blog_view(request,**kwargs):
     posts=Post.objects.filter(published_date__lte=timezone.now(),status=1)
+    if kwargs.get('cat_name')!=None:
+        posts=posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username')!=None:
+        posts=posts.filter(author__username=kwargs['author_username'])
+        
     context={'posts': posts}
     return render(request,'blog/blog-home.html',context)
 
@@ -25,3 +30,20 @@ def blog_single(request,pid):
 
 def test(request):
     return render(request,'test.html')
+
+def blog_category(request,cat_name):
+    posts=Post.objects.filter(status=1)
+    posts=posts.filter(category__name=cat_name)
+    context={'posts': posts}
+    return render(request,'blog/blog-home.html',context)
+    
+
+
+def blog_search(request):
+    posts=Post.objects.filter(status=1)
+    if request.method=='GET':
+        if s:=request.GET.get('s'):
+            posts=posts.filter(content__contains=s)
+            
+    context={'posts': posts}
+    return render(request,'blog/blog-home.html',context)
