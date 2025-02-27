da25564084523c0e90bb9aab5fe7a52b404447f1
Afsaneh Hassani <afsaneh_hassani67@yahoo.com>
2025-02-27 23:09:03 +0330

    using some important modules

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
-    'blog'
+    'blog',
+    'django.contrib.humanize',
+    'django.contrib.sites',
 ]
 
+SITE_ID=2
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
