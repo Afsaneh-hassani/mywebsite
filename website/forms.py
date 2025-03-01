a27c3c029fabef6a640270afed0a015f6db24e26
Afsaneh Hassani <afsaneh_hassani67@yahoo.com>
2025-03-01 21:54:38 +0330

    captcha module

--- website/forms.py
+++ website/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from website.models import Contact, Newsletter 
+from captcha.fields import CaptchaField
 
 
 class NameForm(forms.Form):
@@ -10,6 +11,9 @@ class NameForm(forms.Form):
     
     
 class ContactForm(forms.ModelForm):
+    captcha = CaptchaField()
+    
+    
     class Meta:
         model=Contact
         fields='__all__'
