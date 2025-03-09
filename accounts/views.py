b0b740cf19fe8b2cad3ede85412fcc0d12a6790c
Afsaneh Hassani <afsaneh_hassani67@yahoo.com>
2025-03-09 19:14:26 +0330

    correction of project authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout , views
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.decorators import login_required
-from . forms import CustomUserCreationForm, CustomAuthenticationForm
+from . forms import CustomUserCreationForm, CustomAuthenticationForm 
+from django.contrib import messages
 
 
 # Create your views here.
@@ -16,6 +17,10 @@ def login_view(request):
                 if user is not None:
                     login(request,user)
                     return redirect('/')
+            else:
+            
+                messages.error(request,'USERNAME OR PASSWORD IS INCORRECT')
+                   
             
         form=CustomAuthenticationForm()
         context={'form':form}
--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -128,12 +128,12 @@ AUTHENTICATION_BACKENDS = [
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_PORT = 587
-#EMAIL_USE_TLS = True
-#EMAIL_HOST_USER = 'afsaneh.hs@gmail.com'
-#EMAIL_HOST_PASSWORD = ''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'afsaneh.hs@gmail.com'
+EMAIL_HOST_PASSWORD = ''
 
 
 
